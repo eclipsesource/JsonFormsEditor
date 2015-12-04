e146fdf10d3e873b515b4d580442a10872c6459a
Felix Thiele <felix.thiele@tum.de>
2015-12-04 15:43:20 +0100

    Fixed gruntfile & Remove ID from Properties, change order of label and type

--- app/src/components/tree/model/control.ts
+++ app/src/components/tree/model/control.ts
@@ -1,83 +1,89 @@
 module app.tree {
 
-  export class Control implements TreeElement {
-    private propertiesSchema = {};
-    private propertiesUISchema = {};
-    private propertiesData = {};
+    export class Control implements TreeElement {
+        private propertiesSchema = {};
+        private propertiesUISchema = {};
+        private propertiesData = {};
 
-    constructor(id: number) {
-      // obtained from the "control" part of the metaSchema
-      this.propertiesSchema = {
-        "type": "object",
-        "properties": {
-          "id": {
-            "type": "integer"
-          },
-          "type": {
-            "type": "string",
-            "enum": [
-              "Control"
-            ]
-          },
-          "label": {
-            "type": "string"
-          },
-          "scope": {
-            "type": "object",
-            "properties": {
-              "$ref": {
-                "type": "string"
-              }
-            }
-          }
+        constructor(id:number) {
+            // obtained from the "control" part of the metaSchema
+            this.propertiesSchema = {
+                "type": "object",
+                "properties": {
+                    "label": {
+                        "type": "string"
+                    },
+                    "type": {
+                        "type": "string",
+                        "enum": [
+                            "Control"
+                        ]
+                    },
+                    "scope": {
+                        "type": "object",
+                        "properties": {
+                            "$ref": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                }
+            };
+
+            this.propertiesUISchema = {
+                "type": "VerticalLayout",
+                "elements": [
+                    {
+                        "type": "Control",
+                        "label": "Label",
+                        "scope": {"$ref": "#/properties/label"},
+                    },
+                    {
+                        "type": "Control",
+                        "label": "Type",
+                        "scope": {"$ref": "#/properties/type"},
+                    },
+                    {
+                        "type": "Control",
+                        "label": "Scope",
+                        "scope": {"$ref": "#/properties/scope/properties/$ref"},
+                    }
+                ]
+            };
+
+            this.propertiesData["id"] = id;
+            this.propertiesData["type"] = "Control";
+            this.propertiesData["label"] = "";
+            this.propertiesData["scope"] = "";
         }
-      };
 
-      this.propertiesUISchema = {
-        "type": "VerticalLayout",
-        "elements": [
-          {
-            "type": "Control",
-            "label": "Id",
-            "scope": { "$ref": "#/properties/id" },
-          },
-          {
-            "type": "Control",
-            "label": "Type",
-            "scope": { "$ref": "#/properties/type" },
-          },
-          {
-            "type": "Control",
-            "label": "Label",
-            "scope": { "$ref": "#/properties/label" },
-          },
-          {
-            "type": "Control",
-            "label": "Scope",
-            "scope": { "$ref": "#/properties/scope/properties/$ref" },
-          }
-        ]
-      };
+        getId():number {
+            return this.propertiesData["id"];
+        }
 
-      this.propertiesData["id"] = id;
-      this.propertiesData["type"] = "Control";
-      this.propertiesData["label"] = "";
-      this.propertiesData["scope"] = "";
+        setId(newId:number) {
+            this.propertiesData["id"] = newId;
+        }
+
+        getTitle():string {
+            return "Control";
+        }
+
+        getNodes():TreeElement[] {
+            return null;
+        }
+
+        getPropertiesSchema() {
+            return this.propertiesSchema;
+        }
+
+        getPropertiesUISchema() {
+            return this.propertiesUISchema;
+        }
+
+        getPropertiesData() {
+            return this.propertiesData;
+        }
     }
-
-    getId() : number { return this.propertiesData["id"]; }
-
-    setId(newId: number) { this.propertiesData["id"] = newId; }
-
-    getTitle() : string { return "Control"; }
-
-    getNodes() : TreeElement[] { return null; }
-
-    getPropertiesSchema() { return this.propertiesSchema; }
-
-    getPropertiesUISchema() { return this.propertiesUISchema; }
-
-    getPropertiesData() { return this.propertiesData; }
-  }
 
 }
