865a04aa551deb7eabd1d0c9544197bd31247242
hecforga <kle-tor@hotmail.com>
2015-12-10 19:38:00 +0100

    Remove js and js.map files

--- app/src/components/core/core.module.js
+++
@@ -1,9 +0,0 @@
-var app;
-(function (app) {
-    var core;
-    (function (core) {
-        'use strict';
-        angular.module('app.core', []);
-    })(core = app.core || (app.core = {}));
-})(app || (app = {}));
-//# sourceMappingURL=core.module.js.map
