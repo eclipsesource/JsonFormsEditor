faf5ed18bcd2ab192d6656c919b68995dcf6e384
fraruca <fraruca@inf.upv.es>
2015-12-22 02:42:49 +0100

    Added methods for editing data schema, fixed scrolling and fit to window

--- app/src/components/core/jsonschema/jsonschema.service.ts
+++ app/src/components/core/jsonschema/jsonschema.service.ts
@@ -48,6 +48,7 @@ module app.core.jsonschema{
         }
 
         getFields(): any[]{
+            console.log(this.fields.length);
             return this.fields;
         }
 
@@ -70,13 +71,48 @@ module app.core.jsonschema{
         // eg. : ['person', 'appearance', 'head']
         // returns a boolean indicating if the addition was succesful(when false, it means the element was not added)
         addNewProperty(name: string, content:any, path: string[]) : boolean {
-
+            if(!content.hasOwnProperty('type')){
+                return false;
+            }
             var cur = this.traverseJsonPath(path);
             if(cur==false){
                 return false;
             }
+
+
             cur[name] = content;
+            this.fields.push({
+                name: name,
+                type: content.type
+            });
             return true;
+        }
+
+        addSubfolder(name:string, path: string[]): boolean {
+            var sub = {
+                'type': 'object',
+                'properties': {}
+            }
+
+            var cur = this.traverseJsonPath(path);
+            if(cur == false){
+                return false;
+            }
+
+            cur[name] = sub;
+            return true;
+        }
+
+        removeProperty(name: string, path: string[]): boolean {
+            console.log(name+'  '+path);
+            var cur = this.traverseJsonPath(path);
+
+            console.log(cur);
+            if(cur==false || !cur.hasOwnProperty(name)){
+                return false;
+            }
+
+            return delete cur[name];
         }
 
         getPropertyContent(name: string, path: string[]): any {
@@ -91,15 +127,17 @@ module app.core.jsonschema{
         private traverseJsonPath(path: string[]): any {
             var cur = this.json;
             var index = 0;
+            if(path.length==0){return cur.properties};
             while(index < path.length) {
-                if(cur.hasOwnProperty(path[index])) {
-                    cur = cur[path[index]];
+                if(cur.hasOwnProperty('properties') && cur.properties.hasOwnProperty(path[index])) {
+                    cur = cur.properties[path[index]];
                     index++;
                 } else {
                     // path doesnt exist
                     return false;
                 }
             }
+
             return cur;
         }
     }
--- app/src/components/toolbox/toolbox.service.ts
+++ app/src/components/toolbox/toolbox.service.ts
@@ -4,6 +4,7 @@
 
 module app.toolbox {
 
+    // TODO validation so that there cannot be 2 schema elements with the same name in the same path of the toolbox(nor in dataschema)
     import JsonSchemaService = app.core.jsonschema.JsonSchemaService;
     import MetaSchemaService = app.core.metaschema.MetaSchemaService;
     import GeneralToolboxElement = app.core.GeneralToolboxElement;
@@ -20,7 +21,7 @@ module app.toolbox {
 
         constructor(public jsonSchemaService: JsonSchemaService, public metaSchemaService: MetaSchemaService) {
             this.initializeGeneralElements();
-            this.initializeSchemaElements();
+            this.loadSchemaElements(demoSchema);
         }
 
         private initializeGeneralElements() {
@@ -42,9 +43,9 @@ module app.toolbox {
             }
         }
 
-        private initializeSchemaElements() {
-            //TODO load when user clicks button and use uploaded file
-            this.jsonSchemaService.loadFromJson(demoSchema);
+        private loadSchemaElements(jsonWithDataSchema: any) {
+
+            this.jsonSchemaService.loadFromJson(jsonWithDataSchema);
 
             var schemaProperties:any[] = this.jsonSchemaService.getFields();
             for(var i = 0; i < schemaProperties.length; i++) {
@@ -54,6 +55,85 @@ module app.toolbox {
             }
 
 
+        }
+
+        //adds new data element into schema and into toolbox
+        //returns if the addition was successful
+        //PARAMETERS: content has to be an object containing the property 'type'
+        public addSchemaElement(scope: string, content: any): boolean {
+
+            if(this.jsonSchemaService.getNames().indexOf(scope)!=-1){
+                console.log('ERROR: Trying to add a duplicated schema element');
+                return false;
+            }
+
+            var bundle = this.convertScopeToPathAndName(scope);
+            var name = bundle.name;
+            var path = bundle.path;
+
+
+            //Schema elements always need a type property
+            if(!content.hasOwnProperty('type')){
+                return false;
+            }
+            //if the addition works on the schema, the element gets added into the toolbox array
+            if(this.jsonSchemaService.addNewProperty(name,content,path)) {
+
+                var element: ControlToolboxElement = new ControlToolboxElement(this.convertScopeToLabel(scope),content.type,scope);
+                this.schemaElements.push(element);
+                return true;
+            }
+            return false;
+        }
+
+
+
+        public removeSchemaElement(scope: string): boolean {
+
+            var bundle = this.convertScopeToPathAndName(scope);
+            var name = bundle.name;
+            var path = bundle.path;
+            if(this.jsonSchemaService.removeProperty(name, path)) {
+
+                for(var i = 0; i< this.schemaElements.length; i++){
+                    if(this.schemaElements[i].getScope() == scope) {
+
+                        this.schemaElements.splice(i,1);
+                        return true;
+                    }
+                }
+
+            } else {
+                return false;
+            }
+
+            console.log('ERROR: schema element was removed on the Json Schema, but it could not be found in the toolbox!');
+            return true;
+
+        }
+
+
+        private convertScopeToPathAndName(scope: string): any {
+
+            var path: string[] = scope.split('/');
+            var name = path[path.length-1];
+
+            path.splice(path.length-1,1);
+
+            return {
+                name: name,
+                path: path
+            }
+        }
+
+        private convertPathAndNameToScope(name: string, path: string[]): string {
+            var res = '';
+            res += path.join('/');
+            if(res != ''){
+                res+='/';
+            }
+            res+=name;
+            return res;
         }
 
         private convertScopeToLabel(scope:string):string {
