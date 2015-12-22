5a185d564c25c51b949368cc433d82cc0a738bbf
Felix Thiele <felix.thiele@tum.de>
2015-12-22 10:22:26 +0100

    Implemented ObserverPattern to be used for the tree later on; moved HideIf Directive to directives folder

---
+++ app/src/shared/observer.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../../typings/lodash/lodash.d.ts" />
+
+interface Observer<T> {
+    data : T;
+    update(data:T);
+}
+
+abstract class Observable<T> {
+    private observers : Observer<T>[];
+
+    registerObserver(observer : Observer<T>) : void {
+        this.observers.push(observer);
+    }
+
+    removeObserver(observer : Observer<T>) : void {
+        _.remove(this.observers, observer);
+    }
+
+    notifyObservers(data : T) : void {
+        _.forEach(this.observers, (observer : Observer<T>) => {
+           observer.update(data);
+        });
+    }
+}
