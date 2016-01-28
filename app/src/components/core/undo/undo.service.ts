53c0a67ffe496cfc24e6be53a9c7bbf001571951
Felix Thiele <felix.thiele@tum.de>
2016-01-28 14:25:50 +0100

    Preview now updates on undo

--- app/src/components/core/undo/undo.service.ts
+++ app/src/components/core/undo/undo.service.ts
@@ -2,6 +2,7 @@ module app.core.undo {
     import TreeService = app.tree.TreeService;
     import ToolboxService = app.toolbox.ToolboxService;
     import DetailService = app.detail.DetailService;
+    import PreviewUpdateEvent = app.preview.PreviewUpdateEvent;
     export class UndoService {
 
         private treeServiceMementoStack:TreeServiceMemento[] = [];
@@ -26,6 +27,7 @@ module app.core.undo {
             if(this.canUndo()){
                 this.treeService.setMemento(this.treeServiceMementoStack.pop());
                 this.toolboxService.setMemento(this.toolboxServiceMementoStack.pop());
+                this.treeService.notifyObservers(new PreviewUpdateEvent(null, JSON.parse(this.treeService.exportUISchemaAsJSON())));
             }
         }
 
