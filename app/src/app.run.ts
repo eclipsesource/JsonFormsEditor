d09a7eb3c7892c24d05381d6ca3091dd7cf256c4
Felix Thiele <felix.thiele@tum.de>
2016-02-05 15:46:28 +0100

    Added dialog preventing the app from accidental unloading

--- app/src/app.run.ts
+++ app/src/app.run.ts
@@ -4,12 +4,13 @@ module app {
     import IDialogOptions = angular.material.IDialogOptions;
     import DataschemaImportController = app.dialogs.dataschemaimport.DataschemaImportController;
     import ILocationService = angular.ILocationService;
+    import IWindowService = angular.IWindowService;
 
     class Runner {
 
-        static $inject = ['$mdDialog', '$location'];
+        static $inject = ['$mdDialog', '$location', '$window'];
 
-        constructor($mdDialog:IDialogService, $location:ILocationService) {
+        constructor($mdDialog:IDialogService, $location:ILocationService, $window:IWindowService) {
             if ($location.path() === '') {
                 var options:IDialogOptions = {
                     parent: angular.element(document.body),
@@ -19,6 +20,10 @@ module app {
                 };
 
                 $mdDialog.show(options);
+            }
+
+            $window.onbeforeunload = () => {
+                return "Using the back- or refresh button shuts down this application.";
             }
         }
     }
