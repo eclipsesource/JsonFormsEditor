7308ec2193a9337d78d30d18bfea2afd292b4c53
hecforga <kle-tor@hotmail.com>
2016-04-04 10:44:41 +0200

    Add some tests for Toolbox module

--- app/src/components/toolbox/toolbox.spec.ts
+++ app/src/components/toolbox/toolbox.spec.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../../../typings/jasmine/jasmine.d.ts" />
 /// <reference path="../../../../typings/angularjs/angular-mocks.d.ts" />
 
+import ToolboxService = app.toolbox.ToolboxService;
+import ControlToolboxElement = app.core.model.ControlToolboxElement;
 'use strict';
 
 
@@ -8,6 +10,7 @@ describe('toolbox', ()=> {
 
     //beforeEach(angular.mock.module('temp/ts/templates.js'));
     beforeEach(angular.mock.module('app'));
+    beforeEach(angular.mock.module('app.toolbox'));
 
     it('should delete element when clicking on delete', (inject(($rootScope, $templateCache) => {
 
@@ -18,12 +21,44 @@ describe('toolbox', ()=> {
     it('should add element when creating new one', () => {
 
     });
-    it('when adding element to tree, should not be deletable', () => {
+    it('when adding element to tree, should not be deletable', (inject(($rootScope, ToolboxService) => {
+        var scope = $rootScope.$new();
+        scope.schema = {
+            "type": "object",
+            "properties": {
+                "name": { "type": "string" }
+            }
+        };
 
-    });
-    it("if object has non-deletable childs, it shouldn't be deletable either", () => {
+        ToolboxService.loadSchema(scope.schema);
+        ToolboxService.increasePlacedTimes("name");
+        scope.nameControl = ToolboxService.getElementByScope("name");
 
-    });
+        expect(ToolboxService.canBeRemoved(scope.nameControl)).toBeFalsy();
+    })));
+    it("if object has non-deletable childs, it shouldn't be deletable either", (inject(($rootScope, ToolboxService) => {
+        var scope = $rootScope.$new();
+        scope.schema = {
+            "type": "object",
+            "properties": {
+                "address": {
+                    "type": "object",
+                    "properties": {
+                        "street": { "type": "string" },
+                        "number": { "type": "integer" }
+                    }
+                }
+            }
+        };
+
+        ToolboxService.loadSchema(scope.schema);
+        ToolboxService.accessFolder("address");
+        ToolboxService.increasePlacedTimes("address/properties/street");
+        ToolboxService.previousFolder();
+        scope.addressControl = ToolboxService.getElementByScope("address");
+
+        expect(ToolboxService.canBeRemoved(scope.addressControl)).toBeFalsy();
+    })));
     it('when dragging element to the tree, it should be added to it', () => {
 
     });
